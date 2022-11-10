4930a37a0b1ab2de027d5dbd85ee9b7aea162592
Artem <112809065+Pon4ik191@users.noreply.github.com>
2022-11-10 12:29:51 +0200

    Add files via upload

---
+++ Рекурсія. Факторіал числа.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int Fact(int N)
+{
+	if (N == 0)
+		return 0;
+	if (N == 1)
+		return 1;
+
+	return N * Fact(N - 1);
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	int a;
+
+	cout << "Введіть число: ";
+
+	cin >> a;
+
+	cout << Fact(a) << endl;
+}
+
